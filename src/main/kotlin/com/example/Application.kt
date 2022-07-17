8e93fa063c79117d24e0ea23cf9c57630ab6bb95
Marc Kohaupt <debuglevel@gmail.com>
2022-07-17 13:24:00 +0200

    Add a logging instruction

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,8 +1,11 @@
 package com.example
 
+import mu.KotlinLogging
 import io.micronaut.runtime.Micronaut.*
 
 fun main(args: Array<String>) {
+	val logger = KotlinLogging.logger {}
+	logger.info("Starting up...");
 	run(*args)
 }
 
